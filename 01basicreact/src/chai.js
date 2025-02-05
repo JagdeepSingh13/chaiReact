0fa0b3e079f752d3056f3148111ecbeeaa4bb7d4
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-02-05 21:22:38 +0530

    made basic chai function and naming conventions

---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,6 @@
+// first letter must be in uppercase
+function Chai() {
+  return <h2>chai in React</h2>;
+}
+
+export default Chai;
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,6 +1,13 @@
+import Chai from './chai'
+
 function App() {
   return (
-    <h1>Hello React with Vite</h1>
+    // <h1>Hello React with Vite</h1>
+    // there should be only one main element or div or <>
+    <>
+      <Chai />
+      <p>a para</p>
+    </>
   )
 }
 
