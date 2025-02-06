1ef485b929a1c7e0566534354a8d3b4738bd07df
JagdeepSingh13 <jagdeeps3105@gmail.com>
2025-02-06 18:28:10 +0530

    made custom react and inner working

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,14 +1,15 @@
 import Chai from './chai'
 
 function App() {
+  const username = 'JSingh'
+
   return (
-    // <h1>Hello React with Vite</h1>
     // there should be only one main element or div or <>
     <>
       <Chai />
-      <p>a para</p>
+      <p>Hello {username}</p>
     </>
   )
 }
 
-export default App
+export default App  
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,6 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+// not working as react has defined way to create elements
+const reactElement = {
+    type: "a",
+    props: {
+      href: "https://google.com",
+      target: "_blank",
+    },
+    children: "Click me to visit GOOGLE",
+};
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit GOOGLE</a>
 )
+
+const anotherUser = "JSingh31";
+
+const reactEle = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'Click to visit GOOGLE - ',
+    anotherUser
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    reactEle
+    // <App />
+    // anotherElement
+)
+
+// < /> it is basically a fn. which is executed by react
+ 
